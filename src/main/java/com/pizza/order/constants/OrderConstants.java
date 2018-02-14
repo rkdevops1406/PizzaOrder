0627933658050d49371cf64894e9ef53aee53b1d
Parani Devaraj <pdevaraj@qca-sdc.com>
2018-02-14 16:53:55 -0600

    data file path updated

--- src/main/java/com/pizza/order/constants/OrderConstants.java
+++ src/main/java/com/pizza/order/constants/OrderConstants.java
@@ -5,6 +5,6 @@ package com.pizza.order.constants;
  */
 public class OrderConstants {
 	private OrderConstants(){}
-	public static final String INPUT_FILE ="src/main/resources/sample_data_ordered.txt";
-	public static final String OUTPUT_FILE ="src/main/resources/output.txt";
+	public static final String INPUT_FILE ="data/sample_data_ordered.txt";
+	public static final String OUTPUT_FILE ="data/output.txt";
 }
